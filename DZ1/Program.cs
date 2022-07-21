d528bd0c92b5f7adb9a841454bdcde518d4452a0
Garry <igor_tarasov_84@mail.ru>
2022-07-21 15:30:53 +0500

    start homework task_1/3

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,4 +1,8 @@
 ﻿// See Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 Console.WriteLine("Введите трёхзначное число");
-int numb = Console.ReadLine()
+int numb = Convert.ToInt32 (Console.ReadLine());
+int dig1 = numb/100;
+int dig2 = numb/10 % 10;
+int dig3 = numb%10;
+Console.WriteLine("вторая цифра=>"+dig2);
