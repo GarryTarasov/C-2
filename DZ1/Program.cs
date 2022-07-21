473fa798f08fa7eec41f2878b4446aa34e83f266
Garry <igor_tarasov_84@mail.ru>
2022-07-21 15:34:15 +0500

    end homework task_1

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -2,7 +2,5 @@
 
 Console.WriteLine("Введите трёхзначное число");
 int numb = Convert.ToInt32 (Console.ReadLine());
-int dig1 = numb/100;
 int dig2 = numb/10 % 10;
-int dig3 = numb%10;
 Console.WriteLine("вторая цифра=>"+dig2);
