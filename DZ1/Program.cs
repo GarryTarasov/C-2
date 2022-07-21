451d4035f7a623a7041b7f7f4fd228b32dccca7a
Garry <igor_tarasov_84@mail.ru>
2022-07-21 15:19:35 +0500

    start homework task_1/2

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное число");
+int numb = Console.ReadLine()
