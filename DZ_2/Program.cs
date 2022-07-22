d4198291978c6462da3ab069e35104b468e0ace2
Garry <igor_tarasov_84@mail.ru>
2022-07-22 17:55:37 +0500

    continue homework task_2

--- DZ_2/Program.cs
+++ DZ_2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int numb = Convert.ToInt32 (Console.ReadLine());
+if(numb<99)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else{string str=numb.ToString();
+ Console.WriteLine(           str[2]);
+}
+     
+     
+        
+       
