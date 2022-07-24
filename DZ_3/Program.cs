5084ecfc899335e8a5e02ea0654676541bca0f7a
Garry <igor_tarasov_84@mail.ru>
2022-07-24 14:24:20 +0500

    end homework task_3

--- DZ_3/Program.cs
+++ DZ_3/Program.cs
@@ -2,14 +2,16 @@
 
 Console.WriteLine("Введите цивру (число) в пределе от 1 до 7 включительно");
 int numb = Convert.ToInt32 (Console.ReadLine());
-
-if(numb==6){
-    Console.WriteLine("Выходной");
-}
-else
+if(numb>7)
 {
-    Console.WriteLine("не выходной");
-}
-
-    
-
+    Console.WriteLine("Тебе же сказали, от 1 до 7 включительно. Попробуй еще раз");
+}   
+    else if(numb<1)
+        {
+        Console.WriteLine("Тебе же сказали, от 1 до 7 включительно. Попробуй еще раз");
+        }
+        else if(numb==7)
+        {Console.WriteLine("выходной");}
+        else if(numb==6)
+        { Console.WriteLine("выходной");}
+        else{ Console.WriteLine(" Не выходной");}
